e2763d3f605e207f34f676f8ac228d5d3f3a10a5
whitenoise333 <graciana_amaranto@hotmail.com>
2023-04-13 20:36:09 -0300

    Creating a light-dark theme switcher using Context

--- Advanced React/firstapp/src/App.js
+++ Advanced React/firstapp/src/App.js
@@ -3,6 +3,21 @@ import DessertsList from "./components/DessertsList";
 import FeedbackForm from "./components/forms/FeedbackForm";
 import Form from "./components/forms/Form";
 import RegistrationForm from "./components/forms/registrationform/RegistrationForm";
+import Switch from "./components/SwitchTheme";
+import { ThemeProvider, useTheme} from "./components/ThemeContext";
+
+const DessertStyle = ({children}) => {
+  const {theme} = useTheme();
+  return (
+    <h2
+      style={{
+      color: theme === "light" ? "black" : "white"
+    }}
+    >
+      {children}
+    </h2>
+  );
+};
 
 const desserts = [
   {
@@ -27,24 +42,96 @@ const desserts = [
   },
 ];
 
+//Ejercicio create a light-dark theme switcher
+const Title = ({children}) => {
+  const {theme} = useTheme();
+  return(
+    <h2
+      style={{
+        color: theme === "light" ? "black" : "white"
+      }}
+    >
+      {children}
+    </h2>
+  );
+};
+
+const Paragraph = ({children}) => {
+  const {theme} = useTheme();
+  return(
+    <p
+      style={{
+        color: theme === "light" ? "black" : "white"
+      }}
+    >
+      {children}
+    </p>
+  );
+};
+
+const Content = () => {
+  return(
+    <div>
+      <Paragraph> 
+        We are a pizza loving family. And for years, I searched and searched and
+        searched for the perfect pizza dough recipe. I tried dozens, or more.
+        And while some were good, none of them were that recipe that would
+        make me stop trying all of the others.
+      </Paragraph>
+    </div>
+  ); //no le agrego theme a componentes especificos, sino que lo hago en cada seccion default }(paragraph)
+};
+
+const Header = () => {
+  return(
+    <header>
+      <Title>Little Lemon 🍕</Title>
+      <Switch />
+    </header>
+  );
+};
+
+const Page = () => {
+  return(
+    <div className="Page">
+      <Title>When it comes to dough</Title>
+      <Content />
+    </div>
+  );
+};
+
 //<DessertsList data={desserts} />
 function App() {   //primero uso map, y dentro pongo lo que quiero mostrar de la lista
   const listItems = desserts.map(dessert => {
     const itemText = `${dessert.name} - ${dessert.createdAt}`
     return <li>{itemText}</li>
-  })
+  });
+
+  const {theme} = useTheme();
 
   return (
-    <div className="App">
-      <h2>List of all desserts and created dates:</h2>
-      {listItems}
-      <h2>List of low calorie desserts:</h2>
-      <DessertsList data={desserts}/>
+    <div className="App" style={{backgroundColor: theme === "light" ? "white" : "black"}}>
+      <Header />
+      <Page />
+      <DessertStyle>
+        <h2>List of all desserts and created dates:</h2>
+        {listItems}
+      </DessertStyle>
+       <h2>List of low calorie desserts:</h2> 
+      <DessertsList data={desserts}/> 
       <Form />
       <FeedbackForm />
       <RegistrationForm />
     </div>
-  );
-}
+  ); //DessertsList no apica el theme porque no está dentro del componente que contiene el themeContext 
+};
 
-export default App;
+function Root(){
+  return(
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+};
+
+export default Root;
