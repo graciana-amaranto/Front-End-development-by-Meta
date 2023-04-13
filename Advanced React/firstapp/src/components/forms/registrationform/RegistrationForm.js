4468e0e255c2b05debeccd78ece21dc831302a3e
whitenoise333 <graciana_amaranto@hotmail.com>
2023-04-12 23:04:04 -0300

    React Forms: práctica

--- Advanced React/firstapp/src/components/forms/registrationform/RegistrationForm.js
+++ Advanced React/firstapp/src/components/forms/registrationform/RegistrationForm.js
@@ -1,3 +1,30 @@
+/*
+**Step 1**
+Convert all the elements from the form to controlled components by adding the value and onChange attributes to each input.
+Make sure the password input is obscured.
+
+**Step 2**
+Show an error message if the password is less than 8 characters long and the user has interacted with the input at least once.
+The error message should be displayed below the password input as follows.
+For that, a component called `PasswordErrorMessage` has been provided to you. Your goal is to implement the logic for when to show the error message.
+The password state is a special one that has an additional property called `isTouched`. This property is used to determine if the user has interacted with the input or not.
+
+**Step 3**
+Prevent the default behaviour of the form when the user clicks the submit button.
+
+**Step 4**
+Implement the body of `getIsFormValid` function to return `true` if the form is valid and `false` otherwise. This determines the submit button state. 
+The rules for the form to be valid are as follows:
+- The first name cannot be empty.
+- The email must be a valid email address and can't be empty. A function called `validateEmail` has been already provided for you to check 
+if the email is valid. It returns `true` if the email is valid and `false` otherwise.
+- The password must be at least 8 characters long
+- The role must be either `individual` or `business`
+
+ **Step 5**
+Implement the body of `clearForm` function to clear the form state after a successful submission.
+*/ 
+
 import { useState } from "react";
 import { validateEmail } from "./validateEmail";
 import "./registrationform.css";
